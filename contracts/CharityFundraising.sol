c43f7394da1adb31af8c0614d18b35f2b98e6ead
tsaqifrazin <tsaqif.gojek@gmail.com>
2025-02-07 20:35:03 +0700

    add CharityFundraising contract with campaign creation and donation functionality

--- contracts/CharityFundraising.sol
+++ contracts/CharityFundraising.sol
@@ -3,4 +3,34 @@ pragma solidity ^0.8.0;
 
 contract CharityFundraising {
     // Struct and functions will be added later
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint goal;
+        uint amountRaised;
+        bool active;
+    }
+
+    mapping(uint => Campaign) campaigns;
+    uint public campaignCount;
+
+    event CampaignCreated(uint campaignCount, address owner, string title, string description, uint goal);
+    event DonationReceived(uint campaignCount, address donor, uint amount);
+
+    function createCampaign(string memory _title, string memory _description, uint _goal) public {
+        require(_goal > 0, "Goal must be greater than 0");
+
+        campaignCount++;
+        campaigns[campaignCount] = Campaign(msg.sender, _title, _description, _goal, 0, true);
+        emit CampaignCreated(campaignCount, msg.sender, _title, _description, _goal);
+    }
+
+    function donate(uint _campaignId) public payable {
+        require(campaigns[_campaignId].active, "Campaign is not active");
+        require(msg.value > 0, "Donation must be greater than 0");
+
+        campaigns[_campaignId].amountRaised += msg.value;
+        emit DonationReceived(_campaignId, msg.sender, msg.value);
+    }
 }

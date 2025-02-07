0ebd181accb67c6bfe6bfb94af6b847790c51b3f
tsaqifrazin <tsaqif.gojek@gmail.com>
2025-02-07 20:36:02 +0700

    add initial test for CharityFundraising campaign creation

---
+++ test/CharityFundraising.ts
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("CharityFundraising", function () {
+  it("Should create a new campaign", async function () {
+    // We will implement the test logic tomorrow
+  });
+});
